72060023b746da307fdd943f77235bb804e11162
sudip bhandari <sudip.post@gmail.com>
2020-05-18 00:37:45 +0530

    Added tests for valid parenthesis program

--- src/test/java/com/sudip/code/ValidParenthesisTest.java
+++ src/test/java/com/sudip/code/ValidParenthesisTest.java
@@ -1,5 +1,26 @@
-import static org.junit.Assert.*;
+package com.sudip.code;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class ValidParenthesisTest {
+    private ValidParenthesis validParenthesis;
+
+    @Before
+    public void init(){
+        validParenthesis=new ValidParenthesis();
+    }
+
+
+    @Test
+    public void testSinglePairReturnsTrue(){
+        assert validParenthesis.isValid("()")==true;
+    }
+
+
+    @Test
+    public void testSingleBracket(){
+        assert validParenthesis.isValid(")")==false;
+    }
 
 }
